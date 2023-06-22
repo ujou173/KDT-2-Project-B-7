ef2c72ba3c5a389f4b9d9f725f7b955d3afb4d6d
KwakYounHo <kwakyh951@gmail.com>
2023-06-22 12:25:13 +0900

    Client-Routing을 위한 패키지 설치/설정

--- client/src/component/index.tsx
+++ client/src/component/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { createRoot, Root } from "react-dom/client"
+import { BrowserRouter as Router } from "react-router-dom"
 import App from "./App"
 
 const rootElement: Root = createRoot(document.getElementById("root") as HTMLElement)
-rootElement.render(<App />)
+rootElement.render(
+  <Router>
+    <App />
+  </Router>
+  )
