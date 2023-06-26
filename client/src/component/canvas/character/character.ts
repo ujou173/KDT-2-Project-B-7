45f37e421e9dd6e2916b0a812f727fdb56eca4b9
KwakYounHo <kwakyh951@gmail.com>
2023-06-26 20:58:24 +0900

    keyboard이벤트에서 velocity제거 keyboard이벤트에서 canvas요소가 canvas밖으로 빠져나가지 못하게 수정

--- client/src/component/canvas/character/character.ts
+++ client/src/component/canvas/character/character.ts
@@ -9,27 +9,46 @@ export class Player {
     x: number,
     y: number
   }
-  velocity: {
-    x: number,
-    y: number
+  pressedKey: {
+    [key: string]: boolean
   }
-  isMoving: boolean
-  constructor ( { canvas, ctx, position, velocity }:PlayerProps ) {
+  constructor ( { canvas, ctx, position }:PlayerProps ) {
     this.c = canvas
     this.ctx = ctx
     this.position = position
-    this.velocity = velocity
-    this.isMoving = false
+    this.pressedKey = {}
   }
 
   draw(): void {
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
     this.ctx.fillStyle = 'blue';
     this.ctx.fillRect(this.position.x, this.position.y, pixel, pixel);
   }
-
+  
   update(): void {
     this.draw();
+    if (this.position.x + pixel <= this.c.width - pixel) {
+      if (this.pressedKey.ArrowRight) {
+        this.position.x += pixel;
+        this.pressedKey.ArrowRight = false;
+      }
+    }
+    if (this.position.x > 0) {
+      if (this.pressedKey.ArrowLeft) {
+        this.position.x -= pixel;
+        this.pressedKey.ArrowLeft = false;
+      }
+    }
+    if (this.position.y + pixel <= this.c.height - pixel) {
+      if (this.pressedKey.ArrowDown) {
+        this.position.y += pixel;
+        this.pressedKey.ArrowDown = false;
+      }
+    }
+    if (this.position.y > 0) {
+      if (this.pressedKey.ArrowUp) {
+        this.position.y -= pixel;
+        this.pressedKey.ArrowUp = false;
+      }
+    }
   }
 }
