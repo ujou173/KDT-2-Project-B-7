fa22d8e1f282e3a65cb80253a283f9a4d243ea4d
KwakYounHo <kwakyh951@gmail.com>
2023-06-27 17:23:38 +0900

    main Canvas에 진입하기 전 닉네임 설정하는 페이지(임시)

---
+++ client/src/component/entryCanvas/entryCanvas.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useNavigate, NavigateFunction } from 'react-router-dom';
+
+interface Props {}
+const EntryCanvas: React.FC<Props> = () => {
+  // navigator
+  const navigate: NavigateFunction = useNavigate();
+  // formElement select
+  const formElement = React.useRef<HTMLFormElement>(null);
+  // input's value
+  const [ nickName, setNickName ] = React.useState<string>("");
+  // input -> onChange handler
+  const nickNameHandle: (e: React.ChangeEvent<HTMLInputElement>) => void = React.useCallback(e=>{
+    setNickName(e.target.value)
+  }, [formElement])
+  // form -> onSubmit handler
+  const submitHandler: (e: React.FormEvent<HTMLFormElement>) => void = React.useCallback(e => {
+    navigate('/main', {state: {nickName}})
+  }, [formElement])
+  return (
+    <>
+      <form ref={formElement} onSubmit={submitHandler}>
+        <fieldset>
+          <legend>
+            사용할 닉네임을 입력하세요
+          </legend>
+          <input type="text" maxLength={12} onChange={nickNameHandle} value={nickName} />
+        </fieldset>
+        <input type="submit" value="전송" />
+      </form>
+    </>
+  )
+}
+
+export default EntryCanvas
