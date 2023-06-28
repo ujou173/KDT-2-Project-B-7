9f562028b4beedbf801101a7af4e86528ef29c31
KwakYounHo <kwakyh951@gmail.com>
2023-06-28 18:03:08 +0900

    최상위 소켓모듈 Gateway를 Provider로 추가

--- server/src/socket-server/socket-server.module.ts
+++ server/src/socket-server/socket-server.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SocketServerService } from './socket-server.service';
 import { ChatModule } from './chat/chat.module';
 import { CharacterMoveModule } from './character-move/character-move.module';
+import { SocketServerGateway } from './socket-server.gateway';
 
 @Module({
   imports: [ChatModule, CharacterMoveModule],
-  providers: [SocketServerService]
+  providers: [SocketServerGateway, SocketServerService]
 })
 export class SocketServerModule {}
