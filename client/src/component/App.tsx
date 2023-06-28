34f40f1f2dccc1f1cab78f9754c5edfbcd73881d
KwakYounHo <kwakyh951@gmail.com>
2023-06-28 18:28:22 +0900

    원하던 동작이 아니어서 제거

--- client/src/component/App.tsx
+++ client/src/component/App.tsx
@@ -13,16 +13,6 @@ export const SocketContext = React.createContext({chatSocket, moveSocket, server
 
 interface Props {}
 const App: React.FC<Props> = () => {
-  // get socketID
-  const [ socketID, setSocketID ] = React.useState<string>("");
-  React.useEffect(()=>{
-    serverSocket.emit('enterUser', 'Entering User');
-  }, [])
-  React.useEffect(()=>{
-    serverSocket.on('yourID', (data: string) => {
-      setSocketID(data);
-    })
-  }, [])
   return (
     <>
       <SocketContext.Provider value={{chatSocket, moveSocket, serverSocket}}>
