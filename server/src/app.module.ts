34cbe9cb82b8d6f9b98449c1ff632309bc15df23
KwakYounHo <kwakyh951@gmail.com>
2023-06-30 15:18:13 +0900

    소켓 import 경로 수정

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FallbackModule } from './fallback/fallback.module';
-import { SocketModule } from './socket-server/Socket.module';
+import { SocketModule } from './socket/Socket.module';
 
 @Module({
   imports: [SocketModule, FallbackModule],
