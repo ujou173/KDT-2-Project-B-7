9037c91d082fec6bcd54f5cce8c2ce56206e2adf
KwakYounHo <kwakyh951@gmail.com>
2023-06-30 15:20:09 +0900

    export service했다면 다른 모듈에서 provider로 주입하지 않아도 되는 듯

--- server/src/socket/character-move/character-move.module.ts
+++ server/src/socket/character-move/character-move.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CharacterMoveGateway } from './character-move.gateway';
-import { SocketServerService } from '../socket-server/socket-server.service';
 import { SocketServerModule } from '../socket-server/socket-server.module';
 
 @Module({
   imports: [SocketServerModule],
-  providers: [CharacterMoveGateway, SocketServerService]
+  providers: [CharacterMoveGateway]
 })
 export class CharacterMoveModule {}
