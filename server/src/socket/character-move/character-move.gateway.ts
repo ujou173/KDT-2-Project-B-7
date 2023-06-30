3feb4e3fc3677b855b82a28484aafaee4d122b64
KwakYounHo <kwakyh951@gmail.com>
2023-06-30 19:17:36 +0900

    게임 캐릭터 접속 해제를 서버가 아닌 move소켓에서 처리하도록 변경

--- server/src/socket/character-move/character-move.gateway.ts
+++ server/src/socket/character-move/character-move.gateway.ts
@@ -1,4 +1,4 @@
-import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { SubscribeMessage, WebSocketGateway, WebSocketServer, OnGatewayDisconnect } from '@nestjs/websockets';
 import { serverAddress } from 'common/server-common';
 import { Server, Socket } from 'socket.io';
 import { SocketServerService, UserData } from '../socket-server/socket-server.service';
@@ -9,21 +9,33 @@ import { SocketServerService, UserData } from '../socket-server/socket-server.se
     origin: serverAddress
   }
 })
-export class CharacterMoveGateway {
+export class CharacterMoveGateway implements OnGatewayDisconnect {
   constructor(private readonly socketServerService: SocketServerService) {}
   @WebSocketServer()
   server: Server;
+
+  // default event
+  handleDisconnect(client: Socket) {
+    this.socketServerService.deleteOnlineUser(client.id);
+    this.server.emit('test', `무브 : 자동 아웃 : ${client.id}가 나갔습니다`)
+  }
+
+  // enter user
   @SubscribeMessage('enterUser')
   enterUser(client: Socket, payload: UserData): void {
     client.emit('prevUsers', this.socketServerService.prevUsers());
     this.socketServerService.addOnlineUser({socketID: client.id, info: payload});
     client.broadcast.emit('enterUser', payload)
   }
+
+  // user's moving
   @SubscribeMessage('moveCharacter')
   moveCharacter(client: Socket, payload: {id: string, position: {x: number, y: number}}): void {
     this.socketServerService.positionUpdate({socketID: client.id, position: payload.position});
     client.broadcast.emit('moveCharacter', {id: payload.id, position: payload.position});
   }
+
+  // exit user
   @SubscribeMessage('outConnect')
   disconnect(client: Socket, payload: string) {
     client.disconnect();
--- server/src/socket/socket-server/socket-server.gateway.ts
+++ server/src/socket/socket-server/socket-server.gateway.ts
@@ -1,4 +1,4 @@
-import { WebSocketGateway, WebSocketServer, SubscribeMessage, OnGatewayDisconnect } from '@nestjs/websockets';
+import { WebSocketGateway, WebSocketServer, SubscribeMessage } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { SocketServerService } from './socket-server.service';
 import { serverAddress } from 'common/server-common';
@@ -8,27 +8,27 @@ import { serverAddress } from 'common/server-common';
     origin: serverAddress
   }
 })
-export class SocketServerGateway implements OnGatewayDisconnect {
+export class SocketServerGateway {
   constructor(private readonly socketServerService: SocketServerService) {}
   @WebSocketServer()
   server: Server;
 
-  // default event
-  handleDisconnect(client: Socket) {
-    this.socketServerService.deleteOnlineUser(client.id);
-  }
-
   // event
+  // online user list request
   @SubscribeMessage('getOnline')
   getOnline(client: Socket, payload: string): void {
     const data = this.socketServerService.getOnlineUser();
     client.emit('getOnline', data)
   }
+
+  // nickname check
   @SubscribeMessage('checkNickName')
   possible(client: Socket, payload: string): void {
     const result: boolean = this.socketServerService.checkDuplicationNickName(payload);
     client.emit('checkNickName', result);
   }
+
+  // exit user
   @SubscribeMessage('outConnect')
   disconnectUser(client: Socket, payload: string): void {
     if (payload === 'outCanvas') {
