733e5eed3b8953c986dbea6db298da6dab1612af
KwakYounHo <kwakyh951@gmail.com>
2023-06-30 15:16:47 +0900

    하나의 큰 모듈로 만들었기에 import 수정

--- server/src/socket/socket-server/socket-server.module.ts
+++ server/src/socket/socket-server/socket-server.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { SocketServerService } from './socket-server.service';
-import { ChatModule } from '../chat/chat.module';
-import { CharacterMoveModule } from '../character-move/character-move.module';
 import { SocketServerGateway } from './socket-server.gateway';
 
 @Module({
